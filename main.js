a2eeef23a771114b941ac9fae91bd08fff46f1fe
Nelson Dorador <nelsonalberto.db@gmail.com>
2024-01-13 00:46:53 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+/*Este codigo es para anotar la lista del supermercado para no olvidar el mandado.
+primer bucle for para solicitar los productos a comprar aqui solo ingresare 4, y 
+luego otro bucle for para mostrar los nombres de la lista ingresada en la consola.*/ 
+
+let miLista = [];
+
+for (let i = 1; i <= 4; i++) {
+     let entrada = prompt("Ingresa nombre del producto a comprar " + i);
+  
+    if (entrada === null) {
+        break;
+    }
+
+    miLista.push(entrada);
+}
+
+console.log("Listas de lo que tengo que comprar :");
+for (let i = 0; i < miLista.length; i++) {
+    console.log(miLista[i]);
+}
